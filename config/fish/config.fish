17f154c1bb34d1d7415bffcf08b7cb83ff035423
André Marconi Custódio <66686554+andremarconi@users.noreply.github.com>
2026-03-23 10:34:13 -0300

    Alteração do prompt para o Fish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -43,6 +43,22 @@ switch $TERM
 				exec tmux set-option -g default-shell (which fish) ';' new-session
 			end
 	end
-
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (command -q hostname; and hostname; or hostnamectl hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
 end
 
